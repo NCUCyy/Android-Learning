bbb1bd43a3ad2d40ea5e5db1a6c5f9a4e7a8c413
cyy <2115895728@qq.com>
2023-11-04 23:21:36 +0800

    ViewModel学习：output用LiveData改写

--- app/src/main/java/com/cyy/app/ch04/CyperViewModel.kt
+++ app/src/main/java/com/cyy/app/ch04/CyperViewModel.kt
@@ -1,16 +1,19 @@
 package com.cyy.app.ch04
 
 import android.util.Base64
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
 class CyperViewModel : ViewModel() {
-    fun encodeBase64(content: String): String {
+    val output:MutableLiveData<String> = MutableLiveData<String>()
+    fun encodeBase64(content: String) {
         val bytes = content.toByteArray()
-        return String(Base64.encode(bytes, Base64.DEFAULT))
+        output.value = String(Base64.encode(bytes, Base64.DEFAULT))
     }
 
-    fun decodeBase64(cyperContent: String): String {
-        return String(Base64.decode(cyperContent, Base64.DEFAULT))
+    fun decodeBase64(cyperContent: String) {
+        output.value = String(Base64.decode(cyperContent, Base64.DEFAULT))
     }
 }
--- app/src/main/java/com/cyy/app/ch04/Model_ViewModel.kt
+++ app/src/main/java/com/cyy/app/ch04/Model_ViewModel.kt
@@ -16,7 +16,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,13 +31,14 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyScreen() {
+fun MyScreen(cyperViewModel: CyperViewModel = viewModel()) {
     val input = remember {
         mutableStateOf("")
     }
     val output = remember {
         mutableStateOf("")
     }
+    val context = LocalContext.current as MainActivity
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -47,17 +50,25 @@ fun MyScreen() {
         })
         Row {
             Button(onClick = {
-                output.value = CyperViewModel().encodeBase64(input.value)
+                cyperViewModel.encodeBase64(input.value)
+                /**
+                 * 只有当旋转屏幕（杀死这个context对于的Activity）时，才会触发页面output值的更新————确实能保留
+                 */
+                cyperViewModel.output.observe(context) {
+                    output.value = it
+                }
             }) {
                 Text(text = "加密")
             }
             Button(onClick = {
-                output.value = CyperViewModel().decodeBase64(input.value)
+                cyperViewModel.decodeBase64(input.value)
+                cyperViewModel.output.observe(context) {
+                    output.value = it
+                }
             }) {
                 Text(text = "解密")
             }
         }
-        if (output.value.isNotBlank())
-            Text(text = output.value)
+        Text(text = "${cyperViewModel.output.value}")
     }
 }
