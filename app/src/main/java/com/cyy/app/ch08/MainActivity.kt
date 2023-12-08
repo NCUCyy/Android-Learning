9faf53791c974978b3963725236470f530275399
NCUCYY <2115895728@qq.com>
2023-12-08 18:14:22 +0800

    （Service的问题）只有暂停后才能显示时间和进度

--- app/src/main/java/com/cyy/app/ch08/MainActivity.kt
+++ app/src/main/java/com/cyy/app/ch08/MainActivity.kt
@@ -5,67 +5,139 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.sp
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
-    private var serviceIntent: Intent? = null
-    // 用于数据交换
-    private val conn = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            TODO("Not yet implemented")
-        }
+    private lateinit var serviceIntent: Intent
+    private lateinit var conn: ServiceConnection
+    var running = false
 
-        override fun onServiceDisconnected(name: ComponentName?) {
-            TODO("Not yet implemented")
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        serviceIntent = Intent(this, MusicService::class.java)
 
         setContent {
-            DisplayScreen(playAction = ::playMusic, stopAction = ::stopMusic)
+            val progressState = remember { mutableStateOf(0.0f) }
+            val timerState = remember { mutableStateOf("") }
+
+
+            var handler = object : Handler(Looper.getMainLooper()) {
+                override fun handleMessage(msg: Message) {
+                    super.handleMessage(msg)
+                    if (msg.what == 0x123) {
+                        progressState.value = msg.arg1 / 100.toFloat()
+                        timerState.value = msg.obj.toString()
+                    }
+                }
+            }
+
+            // 用于数据交换
+            conn = object : ServiceConnection {
+                override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                    val binder = service as MusicService.ProgressBinder
+                    thread {
+                        while (running) {
+                            val msg = Message.obtain()
+                            msg.what = 0x123
+                            msg.arg1 = binder.getMusicProgress()
+                            // 把秒转换为时:分的形式
+                            msg.obj = convertTime(binder.getTimer())
+                            handler.sendMessage(msg)
+                        }
+                    }
+                }
+
+                override fun onServiceDisconnected(name: ComponentName?) {
+                    TODO("Not yet implemented")
+                }
+            }
+
+            DisplayScreen(
+                timerState = timerState,
+                progressState = progressState,
+                playAction = ::playMusic,
+                stopAction = ::stopMusic
+            )
         }
     }
 
     private fun playMusic() {
-        serviceIntent = Intent(this, MusicService::class.java)
-//        startService(serviceIntent)
-        bindService(serviceIntent!!, conn, Context.BIND_AUTO_CREATE)
+        running = true
+        bindService(serviceIntent, conn, Context.BIND_AUTO_CREATE)
     }
 
     private fun stopMusic() {
-        if (serviceIntent != null)
-//            stopService(serviceIntent)
-            unbindService(conn)
+        running = false
+        unbindService(conn)
+    }
+
+    private fun convertTime(seconds: Int): String {
+        val m = seconds / 60
+        val s = seconds % 60
+        val ms = if (m < 10) "0$m" else "$m"
+        val ss = if (s < 10) "0$s" else "$s"
+        return "${ms}:${ss}"
     }
 }
 
 @Composable
-fun DisplayScreen(playAction: () -> Unit, stopAction: () -> Unit) {
-    Box(contentAlignment = Alignment.Center) {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            TextButton(onClick = {
-                playAction.invoke()
-            }) {
-                Text(text = "播放", fontSize = 20.sp)
-            }
-            TextButton(onClick = {
-                stopAction.invoke()
-            }) {
-                Text(text = "暂停", fontSize = 20.sp)
+fun DisplayScreen(
+    timerState: MutableState<String>,
+    progressState: MutableState<Float>,
+    playAction: () -> Unit,
+    stopAction: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = timerState.value, fontSize = 24.sp)
+            LinearProgressIndicator(progress = progressState.value)
+            Row(modifier = Modifier.fillMaxWidth()) {
+                TextButton(onClick = {
+                    playAction.invoke()
+                }) {
+                    Text(text = "播放", fontSize = 20.sp)
+                }
+                TextButton(onClick = {
+                    stopAction.invoke()
+                }) {
+                    Text(text = "暂停", fontSize = 20.sp)
+                }
             }
         }
     }
--- app/src/main/java/com/cyy/app/ch08/MusicService.kt
+++ app/src/main/java/com/cyy/app/ch08/MusicService.kt
@@ -3,11 +3,23 @@ package com.cyy.app.ch08
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 import com.cyy.app.R
+import kotlinx.coroutines.runInterruptible
+import kotlin.concurrent.thread
 
 class MusicService : Service() {
     private lateinit var mediaPlayer: MediaPlayer
+    var running = true
+    var timer = 0
+    var musicProgress = 0
+
+    inner class ProgressBinder : Binder() {
+        fun getMusicProgress() = musicProgress
+        fun getTimer() = timer
+    }
+
     override fun onCreate() {
         super.onCreate()
         mediaPlayer = MediaPlayer.create(this, R.raw.hcy)
@@ -30,22 +42,31 @@ class MusicService : Service() {
     /**
      * 方式二：onBind()
      */
-    override fun onBind(intent: Intent): IBinder? {
+    override fun onBind(intent: Intent): IBinder {
         mediaPlayer.setOnPreparedListener {
             mediaPlayer.start()
         }
         mediaPlayer.setOnCompletionListener {
             mediaPlayer.release()
-            stopSelf()
+            timer = 0
         }
-        return null
+        thread {
+            while (running) {
+                Thread.sleep(1000)
+                timer++
+                musicProgress = (100 * mediaPlayer.currentPosition) / mediaPlayer.duration
+                if (musicProgress >= 100)
+                    running = false
+            }
+        }
+        return ProgressBinder()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onUnbind(intent: Intent?): Boolean {
+        running = false
         if (mediaPlayer.isPlaying) {
             mediaPlayer.stop()
-            mediaPlayer.release()
         }
+        return super.onUnbind(intent)
     }
 }
